27e29d9947a02944fb41b0190ca3b212274fb2ba
Foley-N <noahfoleypro@gmail.com>
2025-04-04 23:50:48 -0500

    updated strave

--- strava/strava.py
+++ strava/strava.py
@@ -1,12 +1,28 @@
 import os
 import base64
 import pycurl
+import json
 from dotenv import load_dotenv
 from requests import post, get
 
-def get_token():
 
-    auth_string = CLIENT_ID + ":" + CLIENT_ID
-    auth_bytes = auth_string.encode("utf-8")
-    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+# some basic set up for using the api
+load_dotenv()
+client_id = os.getenv('CLIENT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
 
+auth_string = client_id + ":" + client_secret 
+auth_bytes = auth_string.encode("utf-8")
+auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+
+url = "https://www.strava.com/api/v3/athlete"
+
+def get_activity():
+    act_url = url + "/athlete/activities"
+    ressult = get(act_url)
+    json_result = json.loads(ressult.content)
+    print(json_result)
+    return json_result
+
+get_activity()
